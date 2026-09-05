31be5aeee0414e076da456ae375ad19f62d39dfa
snmousavi <89077940+snmousavii@users.noreply.github.com>
2026-09-05 23:33:49 +0200

    Create BaseVoting.sol

---
+++ BaseVoting.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract BaseVoting {
+    uint256 public yesVotes;
+    uint256 public noVotes;
+
+    mapping(address => bool) public hasVoted;
+
+    function voteYes() external {
+        require(!hasVoted[msg.sender], "Already voted");
+        hasVoted[msg.sender] = true;
+        yesVotes++;
+    }
+
+    function voteNo() external {
+        require(!hasVoted[msg.sender], "Already voted");
+        hasVoted[msg.sender] = true;
+        noVotes++;
+    }
+
+    function totalVotes() external view returns (uint256) {
+        return yesVotes + noVotes;
+    }
+}
